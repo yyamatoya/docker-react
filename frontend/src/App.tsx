3621b03827eea1a2b6a028322183b862fab6db5a
Yuji Yamatoya <yyamatoya@techfirm.co.jp>
2022-11-25 17:40:25 +0900

    components を学習

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
+// function App() {
+//   return (
+//     <div>
+//       <h1>Hello World!</h1>
+//     </div>
+//   );
+// }
+const app = () => { return <div>Hi!</div> }
+const Cat = () => { return <div>cat</div> }
+
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <Cat />
+      <Cat />
+      <Cat />
     </div>
   );
 }
