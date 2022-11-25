b6b60324e76bf31aa119a8885b12c7e2330b33cf
Yuji Yamatoya <yyamatoya@techfirm.co.jp>
2022-11-25 17:49:42 +0900

    props を学ぶ

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
 
+const User = (props: {
+  name: string;
+  age: number;
+}) => {
+  return <div>Hi, I am {props.name}, and {props.age} years old!</div>;
+}
+
 function App() {
+  const profiles = [
+    { name: "Taro", age: 10 },
+    { name: "Hanako", age: 20 },
+    { name: "No Age" },
+  ];
   return (
     <div>
-      <h1>Hello World!</h1>
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
     </div>
   );
+}
+
+User.defaultProps = {
+  age: 1,
 }
 
 export default App;
