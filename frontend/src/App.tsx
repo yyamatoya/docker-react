ee7de2ed7b4dd640e2de0c1d5e024aaff8fc1528
Yuji Yamatoya <yyamatoya@techfirm.co.jp>
2022-11-28 11:44:52 +0900

    stateの学習

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,29 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-
-const User = (props: any) => {
-  return (
-    <div>
-      Hi, I am {props.name}, and {props.age} years old!
-    </div>
-  );
-};
-
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number,
-};
+import React, { Component } from 'react';
 
 function App() {
-  const profiles = [{ name: 'Taro', age: 10 }, { name: 'Hanako', age: 20 }, { name: 'No Age' }];
-  return (
-    <div>
-      {profiles.map((profile, index) => {
-        return <User name={profile.name} age={profile.age} key={index} />;
-      })}
-    </div>
-  );
+  return <Counter></Counter>;
+}
+
+class Counter extends Component {
+  constructor(props: any) {
+    super(props);
+    this.state = { count: 0 };
+  }
+  handlePlusButton = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  handleMinusButton = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <div>count: {this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
